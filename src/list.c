ffc54805019b7a9d99dd4474c2a221ec2125ad27
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-01-17 22:47:10 -0500

    init: add initial DynamicArray

---
+++ src/list.c
@@ -0,0 +1,64 @@
+#include "list.h"
+#include <stdio.h>
+
+DynamicArray* new_dynamic_array(size_t elem_size, int capacity) {
+    DynamicArray* d = malloc(sizeof(DynamicArray));
+    d->length = 0;
+    d->elem_size = elem_size;
+    d->array = malloc(elem_size * capacity);
+    d->capacity = capacity;
+    return d;
+}
+
+// Get the ith element of the array.
+void* get(DynamicArray* d, int i) {
+    if (d != NULL && i >= 0 && i < d->length) {
+        return (int*)d->array + (i * d->elem_size);
+    }
+
+    return NULL;
+}
+
+// Set the ith element of the array to a new value.
+int set(DynamicArray* d, int i, void* new_value) {
+    if (d != NULL && new_value != NULL && i >= 0 && i < d->length) {
+        memcpy((int*)d->array + (i * d->elem_size), new_value, d->elem_size);
+        return 0;
+    }
+    return -1;
+}
+
+// Push back a new value to the array.
+int push_back(DynamicArray* d, void* new_value) {
+    if (d == NULL || new_value == NULL) { return -1; }
+
+    if (d->length == d->capacity) {
+        resize(d);
+    }
+
+    printf("set: %d\n", set(d, d->length, new_value));
+    d->length++;
+
+    return 0;
+}
+
+// Remove and return last element from array.
+void* pop_back(DynamicArray* d) {
+    if (d->length > 0) {
+        return get(d, d->length--);
+    }
+
+    return NULL;
+}
+
+// Resize dynamic array
+int resize(DynamicArray* d) {
+    d->capacity *= CAPACITY_MULT;
+    void* new_array = realloc(d->array, d->elem_size * d->capacity);
+    if (new_array == NULL) {
+        d->capacity /= CAPACITY_MULT;
+        return -1;
+    }
+    d->array = new_array;
+    return 0;
+}
